04e13ac997f6084d0fa319f88678f15b7a77a3cc
Diego Carvalho <diegocarvalho@MacBook-Air-de-Diego.local>
2020-03-12 00:01:10 -0300

    Exercicio1

---
+++ original/curso_js/js/ex1.js
@@ -0,0 +1,4 @@
+
+let nome = 'Diego Carvalho'
+let idade = 27
+console.log(`Olá, eu me chamo ${nome}, tenho ${idade} anos e estou estudando Javascript.`)
--- original/curso_js/js/script.js
+++ original/curso_js/js/script.js
@@ -1,2 +1,21 @@
-//alert("Mensagem enviada pelo ALERT")
-console.log("Mensagem exibida no CONSOLE")
+
+// Tipo de dados null, undefined
+
+//Undefined
+let indefinido
+console.log(indefinido)
+console.log(typeof indefinido)
+
+let nome = "Pedro"
+console.log(nome[5])
+
+//Null
+let temperatura = 35
+console.log(temperatura)
+
+temperatura = null
+console.log(temperatura)
+
+console.log(temperatura === undefined)
+
+console.log(typeof temperatura)
