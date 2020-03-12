09bd51b1d01d3d3b3646e1ed1fac431bbc29ffbc
Diego Carvalho <diegotecinfo@yahoo.com.br>
2020-03-12 13:57:05 -0300

    questao6 exercicio1

--- original/curso_js/js/ex1.js
+++ original/curso_js/js/ex1.js
@@ -2,3 +2,18 @@
 let nome = 'Diego Carvalho'
 let idade = 27
 console.log(`Olá, eu me chamo ${nome}, tenho ${idade} anos e estou estudando Javascript.`)
+
+let aluno = 'Tatiane Aparecida'
+let matricula = 0102530
+let prova1 = 10
+let prova2 = 5
+media = (prova1 + prova2) /2
+
+console.log(`O aluno ${aluno}, matrícula ${matricula}, obteve a média final ${media}.`)
+
+let fone = 961507021
+console.log(fone.toString().length == 9)
+
+console.log(Math.pow(32,6))
+
+// Questão 6
