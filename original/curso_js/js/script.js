e5219ba54a703d89f6dd4d0d250b9c98cd83442b
Diego Carvalho <diegocarvalho@MacBook-Air-de-Diego.local>
2020-03-16 23:26:08 -0300

    funcoes desafio IMC

--- original/curso_js/js/script.js
+++ original/curso_js/js/script.js
@@ -20,9 +20,30 @@ console.log(temperatura)
 console.log(temperatura === undefined)
 
 console.log(typeof temperatura)
-*/
 
+
+// AULA -  DOM
 var caixa_conteudo = document.getElementById("caixa_azul").innerHTML;
 console.log(caixa_conteudo)
 
 document.getElementById("caixa_amarela").innerHTML = `<h1>${caixa_conteudo}`
+*/
+
+//AULA FUNCOES
+function soma_numeros() {
+    var x = 10
+    var y = 5
+    var soma = x + y
+    console.log(soma)
+}
+
+soma_numeros()
+
+function soma_args(num1, num2) {
+    var soma = num1 + num2
+    return soma
+}
+
+console.log(soma_args(10,40))
+
+// 11:41 minutos, exercicio funcao calculo de imc
