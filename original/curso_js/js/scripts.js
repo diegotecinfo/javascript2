ce3cf7485eaa740a04f993bf0a38ed1605ce7856
Diego Carvalho <diegotecinfo@yahoo.com.br>
2020-03-19 13:59:21 -0300

    ARRAYS

--- original/curso_js/js/scripts.js
+++ original/curso_js/js/scripts.js
@@ -46,7 +46,6 @@ function soma_args(num1, num2) {
 
 console.log(soma_args(10,40))
 
-*/
 
 //EXERCICIO IMC
 function calc_imc(peso, altura) {
@@ -60,3 +59,24 @@ var minha_altura = parseFloat(document.getElementById("altura").innerHTML)
 var meu_imc = calc_imc(meu_peso, minha_altura)
 
 document.getElementById("imc").innerHTML = meu_imc.toFixed(2)
+
+
+//AULA 12 - ARRAYS
+
+var alunos = ["João", "Maria", "José"]
+var num_primos = [2, 3, 5, 7, 11, 13]
+
+console.log(alunos.length)
+console.log(num_primos[4])
+*/
+
+var grupos = [ 
+    [ "João" , "Maria" ],
+    [ "Pedro" , "Joana", "André", "Júlio" ],
+    [ "Carolina" , "Helena", "Marcelo" ]
+]
+
+console.log(grupos.length)
+console.log(grupos[1][1])
+
+//ARRAYS 5:43 MINUTOS
